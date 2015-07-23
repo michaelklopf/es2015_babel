f300a2b20f8f5a80d0da9ef9c0ea02a752b36cb5
Klopf Michael <m.klopf@fis-gmbh.de>
2015-07-23 11:38:40 +0200

    Removed semicolons, switched to gulp-babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-var gulp = require('gulp');
-var browserify = require('gulp-browserify');
-var concat = require('gulp-concat');
-var using = require('gulp-using');
+var gulp = require('gulp')
+var babel = require("gulp-babel")
+var concat = require('gulp-concat')
+var using = require('gulp-using')
 
 gulp.task('browserify', function() {
     gulp.src('jsbabel/main.js')
       .pipe(using())
-      .pipe(browserify({transform: 'babelify'}))
+      .pipe(babel())
       .pipe(concat('main.js'))
-      .pipe(gulp.dest('./dist/js'));
-});
+      .pipe(gulp.dest('./dist/js'))
+})
 
 gulp.task('copy', function() {
     gulp.src('views/index.html')
-    .pipe(using())
-      .pipe(gulp.dest('./dist'));
-});
+      .pipe(using())
+      .pipe(gulp.dest('./dist'))
+})
 
-gulp.task('default',['browserify', 'copy']);
+gulp.task('default', ['browserify', 'copy'])
 
 gulp.task('watch', function() {
-    gulp.watch('static/**/*.*', ['default']);
-});
+    gulp.watch('static/**/*.*', ['default'])
+})
