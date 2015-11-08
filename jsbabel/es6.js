6e03f98ff7f64445f17fc43e62823270bd3ae32e
Michael Klopf <klopf.michael@gmail.com>
2015-11-08 12:36:30 +0100

    fun with loops

--- jsbabel/es6.js
+++ jsbabel/es6.js
@@ -15,3 +15,50 @@ function aCondition(condition) {
 
 console.log(aCondition(true))
 console.log(aCondition(false))
+
+// let vs Swift let
+let colorBlack = "Black"
+colorBlack = "Blue" // does not behave like the Swift let
+console.log(colorBlack)
+
+// const is the Swift let analogue
+const RED = "red"
+console.log(RED)
+
+// objects as const with editable attributes
+const address = {
+  street: "Main Street",
+  countryCode: "12345"
+}
+
+address.street = "Side Street"
+address.countryCode = "54321"
+
+console.dir(address)
+
+// better loop
+for (let i = 0; i < 3; i++) {
+  console.log(i)
+}
+
+var funcs = []
+for (var i = 0; i < 3; i++) {
+  funcs.push(function() {
+    console.log(i)
+  })
+}
+
+funcs.forEach(function(func) {
+  func()
+})
+
+funcs = []
+for (let i = 0; i < 3; i++) {
+  funcs.push(function() {
+    console.log(i)
+  })
+}
+
+funcs.forEach(function(func) {
+  func()
+})
